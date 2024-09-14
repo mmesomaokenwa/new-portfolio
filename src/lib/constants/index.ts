8d293f2ca80242031d14f608f89c87a781e71a40
Mmesoma Okenwa <godsfavourokenwa12@gmail.com>
2024-09-14 06:08:22 +0100

    edit projects

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -101,11 +101,11 @@ export const projects = [
   // },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "Imaginify - AI Image SaaS",
+    des: "A REAL Software-as-a-Service app with AI features for image generation and editing and a payments and credits system using the latest tech stack.",
     img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://imaginify-ai-prod.vercel.app",
   },
   {
     id: 4,
